fcef3e6d010fe9e28469409d4996f32e78ce840c
estebanzeb <estebanzeb@gmail.com>
2021-09-18 09:56:23 -0500

    Se crea App para ver estilos

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
+import { bold } from 'ansi-colors';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Estilos.container}>
+        <Text>Bienvenido a mi app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+
+        <View>
+            <Text style={[Estilos.azulGrande]}>hola</Text>
+            <Text style={[Estilos.azulGrande,Estilos.colorRojo]}>App de pruebas</Text>
+        </View>
+        <View>
+          <Button title="Presionar" onPress={()=>Alert.alert('Presiono boton')}></Button>
+        </View>
+        <View>
+          <TextInput>Ingresa</TextInput>
+        </View>
     </View>
+
+    
   );
 }
 
-const styles = StyleSheet.create({
+const Estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  azulGrande:{
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:50,
+  },
+  colorRojo:{
+    color:'red',
+  },
+  correoInput:{
+    underlineColorAndroid: 'Transparent',
+    placeholder:'Email',
+    onChangeText: this.handleEmail,
+  },
+  datoNumerico:{
+
+  },
+  alfanumerico:{
+
+  },
+  passwordC:{
+    underlineColorAndroid: 'Transparent',
+    placeholder:'Contraseña',
+    placeholderTextColor:'green',
+    onChangeText:this.handlePassword,
+  },
+
+
 });
+
+
